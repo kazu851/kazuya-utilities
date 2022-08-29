cc3a9400e2475fb0a34f6df24d3790068394231c
Jean Carlos Cochrane Villegas <jean.cochran@quiputech.com>
2022-08-28 21:38:42 -0500

    config file reload 100%

--- src/main/java/com/hotmail/jean_cochrane/kazuya_utilities/Main.java
+++ src/main/java/com/hotmail/jean_cochrane/kazuya_utilities/Main.java
@@ -6,19 +6,16 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.hotmail.jean_cochrane.MainCommand;
+
+
 // https://www.youtube.com/watch?v=Hs1kZu1_ruI
 // https://github.com/2lstudios-mc/Bukkit-Boilerplate
 
 public class Main extends JavaPlugin {
     public void onEnable(){
 
-        final FileConfiguration config = this.getConfig();
-
-        config.addDefault("wellcome-message-active", true);
-        config.addDefault("wellcome-message", "Bienvenido al complemento de Kazuya");
-        config.addDefault("plugin-info-message-active", true);
-        config.options().copyDefaults(true);
-        saveConfig();
+        loadConfig();
 
         Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "========================================================");
         Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "========================================================");
@@ -30,5 +27,22 @@ public class Main extends JavaPlugin {
         pm.registerEvents( new ConfigListener(this), this);
         pm.registerEvents( new PlayerJoin(), this);
         pm.registerEvents( new ReadChat(), this);
+        
+        getCommand("kazuya-utilities").setExecutor(new MainCommand());
     }
+
+
+
+    public void loadConfig(){
+
+        FileConfiguration config = this.getConfig();
+
+        config.addDefault("wellcome-message-active", true);
+        config.addDefault("wellcome-message", "Bienvenido al complemento de Kazuya");
+        config.addDefault("plugin-info-message-active", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+    }
+
 }
